ed59d1f921009ff9c04cbe03b582b22f98dc6784
oxc <524112857@qq.com>
2019-11-07 16:12:53 +0800

    config: commitlint

--- src/containers/views/index.tsx
+++ src/containers/views/index.tsx
@@ -3,9 +3,7 @@ import React from 'react'
 import styles from './index.scss'
 
 const Test = () => {
-    return (
-        <div className={styles.test}>123</div>
-    )
+    return <div className={styles.test}>123</div>
 }
 
 export default Test
