8684f1ea13d0936ca78a669f1e0160b1e051de00
oxc <524112857@qq.com>
2019-11-11 16:08:50 +0800

    feat: 登录注册弹框

---
+++ src/containers/shared/BlogLayout/AuthModal.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react'
+import { Modal, Form, Input, Button, Icon, message } from 'antd'
+import { ModalProps } from 'antd/lib/modal'
+import { FormComponentProps } from 'antd/lib/form'
+
+const FormItem = Form.Item
+
+interface IProps extends FormComponentProps, ModalProps {
+    authModalType: string
+    closeModal: () => void
+    triggerAuthModal: (visible: boolean, type?: string) => void
+}
+
+const AuthModal = ({ visible, form, authModalType, closeModal, triggerAuthModal }: IProps) => {
+    const { getFieldDecorator, validateFields } = form
+
+    const [loading, setLoading] = useState<boolean>(false)
+
+    const submit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        setLoading(true)
+        validateFields(async (err, value) => {
+            if (!err) {
+                setLoading(true)
+                try {
+                    setLoading(false)
+                    // triggerAuthModal(false)
+                    console.log(err, value)
+                } catch (error) {
+                    setLoading(false)
+                }
+            } else {
+                console.log(err)
+            }
+        })
+    }
+
+    return (
+        <Modal
+            onCancel={closeModal}
+            width={320}
+            footer={null}
+            destroyOnClose
+            maskClosable={false}
+            title={authModalType}
+            visible={visible}
+        >
+            <Form onSubmit={submit} layout="horizontal">
+                <FormItem hasFeedback>
+                    {getFieldDecorator('username', {
+                        rules: [
+                            {
+                                required: true,
+                                message: '需要输入用户名'
+                            }
+                        ]
+                    })(
+                        <Input
+                            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            placeholder="请输入用户名"
+                        />
+                    )}
+                </FormItem>
+                <FormItem hasFeedback>
+                    {getFieldDecorator('password', {
+                        rules: [
+                            {
+                                required: true,
+                                message: '需要输入密码'
+                            }
+                        ]
+                    })(
+                        <Input
+                            type="password"
+                            placeholder="请输入密码"
+                            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                        />
+                    )}
+                </FormItem>
+                <FormItem>
+                    <Button loading={loading} type="primary" htmlType="submit" block>
+                        {authModalType === 'login' ? '登录' : '注册'}
+                    </Button>
+                </FormItem>
+            </Form>
+        </Modal>
+    )
+}
+
+export default Form.create<IProps>()(AuthModal)
--- src/containers/shared/BlogLayout/UserInfo.tsx
+++ src/containers/shared/BlogLayout/UserInfo.tsx
@@ -1,19 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from 'antd'
 
 import styles from './index.scss'
+import AuthModal from './AuthModal'
 
 const UserInfo = () => {
+    const [authVisible, setAuthVisible] = useState<boolean>(false)
+    // 弹框类型
+    const [authModalType, setAuthModalType] = useState<string>('login')
+
+    const triggerAuthModal = (visible: boolean, type?: string) => {
+        setAuthVisible(visible)
+        setAuthModalType(type)
+    }
+
     return (
         <div className={styles.headerUserInfo}>
             <>
-                <Button ghost type="primary" size="small" style={{ marginRight: 20 }}>
+                <Button
+                    onClick={() => triggerAuthModal(true, 'login')}
+                    ghost
+                    type="primary"
+                    size="small"
+                    style={{ marginRight: 20 }}
+                >
                     登录
                 </Button>
-                <Button ghost type="danger" size="small">
+                <Button onClick={() => triggerAuthModal(true, 'register')} ghost type="danger" size="small">
                     注册
                 </Button>
             </>
+            <AuthModal
+                triggerAuthModal={triggerAuthModal}
+                closeModal={() => setAuthVisible(false)}
+                authModalType={authModalType}
+                visible={authVisible}
+            />
         </div>
     )
 }
