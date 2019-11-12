24af0d465e9d426aaa77ded26a831ab6877c75d1
oxc <524112857@qq.com>
2019-11-12 18:40:23 +0800

    test: 测试自动部署

--- src/containers/views/AboutMe/index.tsx
+++ src/containers/views/AboutMe/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AboutMe = () => {
     return (
         <div>
-            <div>AbousdgfsdfasdasdasdtMe</div>
+            <div>测测测sdasdasdtMe</div>
         </div>
     )
 }
