7888334728e4e6165d83d56273fdb91c069c1777
oxc <524112857@qq.com>
2019-11-14 11:10:31 +0800

    test: 抽离react测试

--- build/optimization.js
+++ build/optimization.js
@@ -18,12 +18,19 @@ module.exports = isDev
                       name: 'commons',
                       chunks: 'initial'
                   },
+                  react: {
+                        test: /[\\/]node_modules[\\/](react|react-dom|react-router-dom)[\\/]/,
+                        name: 'react',
+                        priority: 8,
+                        reuseExistingChunk: false
+                    },
                   antd: {
                       name: 'antd',
                       test: /[\\/]node_modules[\\/](antd)[\\/]/,
                       chunks: 'all',
                       priority: 9
                   },
+
                   vendor: {
                       name: 'vendor',
                       test: /[\\/]node_modules[\\/](moment|axios)[\\/]/,
@@ -44,63 +51,3 @@ module.exports = isDev
               })
           ]
       }
-
-// module.exports = isDev
-//     ? {}
-//     : {
-//           minimize: true,
-//           minimizer: [
-//               new TerserPlugin({
-//                   terserOptions: {
-//                       parse: {
-//                           ecma: 8
-//                       },
-//                       compress: {
-//                           ecma: 5,
-//                           warnings: false,
-//                           comparisons: false,
-//                           inline: 2
-//                       },
-//                       mangle: {
-//                           safari10: true
-//                       },
-//                       output: {
-//                           ecma: 5,
-//                           comments: false,
-//                           ascii_only: true
-//                       }
-//                   },
-//                   cache: true,
-//                   sourceMap: false
-//               }),
-//               new OptimizeCSSAssetsPlugin({
-//                   cssProcessorOptions: {
-//                       map: false 
-//                   }
-//               })
-//           ],
-//           splitChunks: {
-//               chunks: 'all',
-//               name: false,
-//               cacheGroups: {
-//                   commons: {
-//                       name: 'commons',
-//                       chunks: 'initial',
-//                       minChunks: 2
-//                   },
-//                   'react-vendor': {
-//                         test: /[\\/]node_modules[\\/](react|react-dom|redux|react-router-dom)[\\/]/,
-//                         name: 'react-vendor',
-//                         priority: 3,
-//                         reuseExistingChunk: false
-//                   },
-//                   'antd-vendor': {
-//                     test: /[\\/]node_modules[\\/](antd)[\\/]/,
-//                     name: 'antd-vendor',
-//                     priority: 2,
-//                     reuseExistingChunk: false
-//                   }
-//               }
-//           },
-//           runtimeChunk: true
-//       }
