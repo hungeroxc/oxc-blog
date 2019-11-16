c248d6806b9b1fc54eb977784c9f263599b1988c
oxc <524112857@qq.com>
2019-11-16 17:02:26 +0800

    test: jekins test

--- src/containers/views/ArticleList/index.tsx
+++ src/containers/views/ArticleList/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ArticleList = () => {
     return (
         <div>
-            <div>ArticleList</div>
+            <div>ArticleListqwdqwdqwdqwd</div>
         </div>
     )
 }
