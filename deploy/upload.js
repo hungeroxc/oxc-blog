c3a1f2a3b752becae8a30350af9579c4d461074e
oxc <524112857@qq.com>
2019-12-04 15:01:00 +0800

    test: 自动上传七牛

---
+++ deploy/upload.js
@@ -0,0 +1,75 @@
+// @ts-nocheck
+
+// 只针对正式环境
+
+const fs = require('fs')
+const qiniu = require('qiniu')
+
+// const accessKey = process.QINIUAK
+// const secretKey = process.QINIUSK
+
+const accessKey = 'ub6kVf__74nIJU7rJ9MBUxPTqoE1wSwLP4bXYBHE'
+const secretKey = 'C-Ua9VVgFsoQrNvrwCqz2gQEw9N22VjoKVcj-fIz'
+
+const mac = new qiniu.auth.digest.Mac(accessKey, secretKey)
+const staticPath = 'dist/prod/static'
+const bucket = 'oxc-blog'
+
+const config = new qiniu.conf.Config()
+config.zone = qiniu.zone.Zone_z2
+const formUploader = new qiniu.form_up.FormUploader(config)
+let putExtra = new qiniu.form_up.PutExtra()
+putExtra = null
+
+// 文件上传
+const uploadFile = localFile => {
+    // 配置路径
+    const key = localFile.replace(staticPath + '/', '')
+    console.log(key)
+    const options = {
+        scope: `${bucket}:${key}`
+    }
+    const putPolicy = new qiniu.rs.PutPolicy(options)
+    // 生成凭证
+    const uploadToken = putPolicy.uploadToken(mac)
+    // 上传文件
+    formUploader.putFile(uploadToken, key, localFile, putExtra, (respErr, respBody, respInfo) => {
+        if (respErr) throw respErr
+        if (respInfo.statusCode == 200) {
+            console.log(respBody)
+        } else {
+            console.log(respInfo.statusCode)
+            console.log(respBody)
+        }
+    })
+}
+
+// 目录上传
+const uploadDir = dirPath => {
+    fs.readdir(dirPath, (err, files) => {
+        if (err) throw err
+
+        files.forEach(item => {
+            const path = `${dirPath}/${item}`
+
+            fs.stat(path, (err, state) => {
+                if (err) throw err
+
+                if (state.isDirectory()) {
+                    uploadDir(path)
+                } else {
+                    uploadFile(path)
+                }
+            })
+        })
+    })
+}
+
+fs.exists(staticPath, exists => {
+    if (!exists) {
+        console.log('目录不存在', staticPath)
+    } else {
+        console.log('开始上传')
+        uploadDir(staticPath)
+    }
+})
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,10 @@
+// development  production
+
 export const baseUrls = {
     development: {
         BASE_URL: 'http://127.0.0.1:3000/api'
     },
     production: {
-        BASE_URL: 'http://blog.oxcblog.club:3000/api'
+        BASE_URL: 'http://oxcblog.club:3000/api'
     }
 }
