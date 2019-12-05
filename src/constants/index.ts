f96190385ad0344d82250276208cc2d48a7e56fc
oxc <524112857@qq.com>
2019-12-05 10:36:56 +0800

    test: 七牛部署

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,6 +5,6 @@ export const baseUrls = {
         BASE_URL: 'http://127.0.0.1:3000/api'
     },
     production: {
-        BASE_URL: 'http://111.230.107.109:3000/api'
+        BASE_URL: 'https://blog.oxcblog.club:3000/api'
     }
 }
