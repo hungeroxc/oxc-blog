75fd29cbf8875c3da062091c0641eb42bf1b65e1
oxc <524112857@qq.com>
2019-12-05 21:23:03 +0800

    config: iconfont外链使用https,否则在https协议下出现blocked:maxed-content

--- build/constants.js
+++ build/constants.js
@@ -4,7 +4,7 @@ const FILE_EXTENSIONS = ['.ts', '.tsx', '.js', '.jsx']
 const IS_DEV = process.env.NODE_ENV === 'development'
 const APP_ENV = process.env.APP_ENV
 
-const ICONFONT_SRC_URL = 'http://at.alicdn.com/t/font_587439_6381m1obx75.js'
+const ICONFONT_SRC_URL = 'https://at.alicdn.com/t/font_587439_6381m1obx75.js'
 const HIGHLIGHT_SRC_URL = 'https://cdn.bootcss.com/highlight.js/9.15.6/highlight.min.js'
 
 module.exports = {
