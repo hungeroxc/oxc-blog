d87d49418a1eb2872db7aa8c2a7c84121922e0ba
oxc <524112857@qq.com>
2019-12-05 16:03:42 +0800

    test: 测试环境搭建

--- src/containers/views/AboutMe/index.tsx
+++ src/containers/views/AboutMe/index.tsx
@@ -5,7 +5,7 @@ import styles from './index.scss'
 const AboutMe = () => {
     return (
         <div className={styles.aboutMe}>
-            <div>测sdasdasdtMe</div>
+            <div>测</div>
         </div>
     )
 }
