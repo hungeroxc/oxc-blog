fcde1eb12c707d61afde69bbf1c7b52ac19d81d2
oxc <524112857@qq.com>
2019-12-05 09:58:49 +0800

    test: 七牛部署

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,10 +1,10 @@
 // development  production
 
 export const baseUrls = {
-    production: {
+    development: {
         BASE_URL: 'http://127.0.0.1:3000/api'
     },
-    development: {
+    production: {
         BASE_URL: 'http://111.230.107.109:3000/api'
     }
 }
