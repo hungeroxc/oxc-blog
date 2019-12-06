7707eb09f8dbe6c67c958f403f1e396a27528af8
oxc <524112857@qq.com>
2019-12-06 16:19:51 +0800

    config: 测试环境接口的端口变为3001

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,7 +5,7 @@ export const baseUrls = {
         BASE_URL: 'http://127.0.0.1:3000/api'
     },
     qa: {
-        BASE_URL: 'http://oxcblog.club:3000/api'
+        BASE_URL: 'http://oxcblog.club:3001/api'
     },
     prod: {
         BASE_URL: 'https://oxcblog.club/api'
