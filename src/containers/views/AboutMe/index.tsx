1f874c286d156c23df5cc11ba9341c4a46182e3d
oxc <524112857@qq.com>
2019-12-07 11:04:46 +0800

    feat: 编辑和新增文章时候支持黏贴图片直传七牛

--- src/containers/views/AboutMe/index.tsx
+++ src/containers/views/AboutMe/index.tsx
@@ -5,7 +5,7 @@ import styles from './index.scss'
 const AboutMe = () => {
     return (
         <div className={styles.aboutMe}>
-            <div>测试部署效果sssa</div>
+            <div>测试部署效果,jenkins同时部署多个任务直接崩溃</div>
         </div>
     )
 }
