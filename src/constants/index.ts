a66b4d151aac273c6cfd78a02b1b084f91737c32
oxc <524112857@qq.com>
2020-02-12 17:24:11 +0800

    test: 测试jenkins

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,17 +1,5 @@
 // development  production
 
-// export const baseUrls: PlainObj = {
-//     dev: {
-//         BASE_URL: 'http://127.0.0.1:3001/api'
-//     },
-//     qa: {
-//         BASE_URL: 'http://oxcblog.club:3001/api'
-//     },
-//     prod: {
-//         BASE_URL: 'https://oxcblog.club/api'
-//     }
-// }
-
 export const baseUrls: PlainObj = {
     dev: {
         BASE_URL: 'https://oxcblog.club/api'
