2c9a3900547f9ff63ea677da9685e3fdda65982a
oxc <524112857@qq.com>
2020-02-24 10:01:55 +0800

    fix: 使用心得hightlight  cdn地址

--- build/constants.js
+++ build/constants.js
@@ -5,7 +5,7 @@ const IS_DEV = process.env.NODE_ENV === 'development'
 const APP_ENV = process.env.APP_ENV
 
 const ICONFONT_SRC_URL = 'https://at.alicdn.com/t/font_587439_u4qlz4zvzyq.js'
-const HIGHLIGHT_SRC_URL = 'https://cdn.bootcss.com/highlight.js/9.15.6/highlight.min.js'
+const HIGHLIGHT_SRC_URL = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.18.1/highlight.min.js'
 
 module.exports = {
     FILE_EXTENSIONS,
